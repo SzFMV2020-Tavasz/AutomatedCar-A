4567b10a2c3d94ed8a6765805c3501ae6cbda5ad
Abyss <davidson996@gmail.com>
2020-02-28 01:14:35 +0100

    #59 Add Powertrain Component

---
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/powertrain/PowerTrain.java
@@ -0,0 +1,16 @@
+package hu.oe.nik.szfmv.automatedcar.powertrain;
+
+import hu.oe.nik.szfmv.automatedcar.systemcomponents.SystemComponent;
+import hu.oe.nik.szfmv.automatedcar.virtualfunctionbus.VirtualFunctionBus;
+
+public class PowerTrain extends SystemComponent {
+
+    public PowerTrain(VirtualFunctionBus virtualFunctionBus) {
+        super(virtualFunctionBus);
+    }
+
+    @Override public void loop() {
+
+    }
+
+}
