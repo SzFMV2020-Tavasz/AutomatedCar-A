f99910005cc9d775e0ab9d8d83c0b69c7ec4d8c7
Abyss <davidson996@gmail.com>
2020-02-28 01:33:23 +0100

    #59 Added Some JavaDoc

--- src/main/java/hu/oe/nik/szfmv/automatedcar/powertrain/PowerTrain.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/powertrain/PowerTrain.java
@@ -3,6 +3,8 @@ package hu.oe.nik.szfmv.automatedcar.powertrain;
 import hu.oe.nik.szfmv.automatedcar.systemcomponents.SystemComponent;
 import hu.oe.nik.szfmv.automatedcar.virtualfunctionbus.VirtualFunctionBus;
 
+/**<p>The powertrain encompasses every component that converts the engine’s power into movement.</p>
+<p>This includes the engine, transmission, the driveshaft, differentials, axles; basically anything from the engine through to the rotating wheels.</p>*/
 public class PowerTrain extends SystemComponent {
 
     public PowerTrain(VirtualFunctionBus virtualFunctionBus) {
