1fb2f491f2c47703e67683ca913ede37259fb8ff
Zsuzsanna Szebeni <szebeni.zsuzsanna@gmail.com>
2020-03-05 19:46:01 +0100

    Added previously uncommited test changes for DisplayObjectTest #21

--- src/test/java/hu/oe/nik/szfmv/automatedcar/visualization/DisplayObjectTest.java
+++ src/test/java/hu/oe/nik/szfmv/automatedcar/visualization/DisplayObjectTest.java
@@ -2,7 +2,7 @@ package hu.oe.nik.szfmv.automatedcar.visualization;
 
 import hu.oe.nik.szfmv.automatedcar.AutomatedCar;
 import hu.oe.nik.szfmv.automatedcar.model.WorldObject;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,15 +12,17 @@ public class DisplayObjectTest {
     private DisplayObject displayObject;
     private WorldObject worldObject;
     private AutomatedCar automatedCar;
+    private int numberOfDecimals = 5;
+    private int forPrecision = (int)Math.pow(10, numberOfDecimals);
 
     /**
      * Setting up the test.
      */
-    @Before
+    @BeforeEach
     public void init() {
-        automatedCar = new AutomatedCar(300, 200, "car_2_red.png");
+        automatedCar = new AutomatedCar(292, 230, "car_2_red.png");
         automatedCar.setRotation((float) Math.PI / 4);  // 45
-        worldObject = new WorldObject(441, 200, "road_2lane_90right.png");
+        worldObject = new WorldObject(399, 540, "road_2lane_90right.png");
         displayObject = new DisplayObject(worldObject, automatedCar);
     }
 
@@ -30,8 +32,8 @@ public class DisplayObjectTest {
      */
     @Test
     public void relativePosition() {
-        assertEquals(613, displayObject.getX());
-        assertEquals(247, displayObject.getY());
+        assertEquals(241, displayObject.getX());
+        assertEquals(645, displayObject.getY());
     }
 
     /**
@@ -40,7 +42,18 @@ public class DisplayObjectTest {
      */
     @Test
     public void rotation() {
-        assertEquals((float) Math.PI / 4, displayObject.getRotation());
+        assertEquals((float) Math.round(Math.PI / 4  * forPrecision)/forPrecision,
+                (float)Math.round(displayObject.getRotation() * forPrecision)/forPrecision);
+    }
+
+    /**
+     * Check whether the translation values needed for fixing the position difference caused by the difference between
+     * the displayObject's reference point and the displayObject's image's rotation origo are actually right.
+     */
+    @Test
+    public void rotationDisplacement() {
+        assertEquals(473, displayObject.getRotationDisplacementX());
+        assertEquals(-93, displayObject.getRotationDisplacementY());
     }
 }
 
