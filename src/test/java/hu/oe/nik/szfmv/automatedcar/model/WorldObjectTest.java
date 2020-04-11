e7b283cfd4ec2b00a85e36e16fce3289a8b56d1f
ArchiCat - Szebeni Zsuzsanna <szebeni.zsuzsanna@gmail.com>
2020-04-11 09:33:10 +0200

    Added highlight switch for all sensors in WorldObject #120
    Co-authored-by: DD28RM <westel9000@gmail.com>

--- src/test/java/hu/oe/nik/szfmv/automatedcar/model/WorldObjectTest.java
+++ src/test/java/hu/oe/nik/szfmv/automatedcar/model/WorldObjectTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class WorldObjectTest {
 
@@ -18,6 +20,27 @@ public class WorldObjectTest {
     void addition() {
         System.out.println(worldObject.getX());
         assertEquals(100, worldObject.getX());
+    }
+
+    @Test
+    void setRadarHighlighted() {
+        assertFalse(worldObject.isHighlightedWhenRadarIsOn());
+        worldObject.setHighlightedWhenRadarIsOn(true);
+        assertTrue(worldObject.isHighlightedWhenRadarIsOn());
+    }
+
+    @Test
+    void setCameraHighlighted() {
+        assertFalse(worldObject.isHighlightedWhenCameraIsOn());
+        worldObject.setHighlightedWhenCameraIsOn(true);
+        assertTrue(worldObject.isHighlightedWhenCameraIsOn());
+    }
+
+    @Test
+    void setUltrasoundHighlighted() {
+        assertFalse(worldObject.isHighlightedWhenUltrasoundIsOn());
+        worldObject.setHighlightedWhenUltrasoundIsOn(true);
+        assertTrue(worldObject.isHighlightedWhenUltrasoundIsOn());
     }
 
 }
