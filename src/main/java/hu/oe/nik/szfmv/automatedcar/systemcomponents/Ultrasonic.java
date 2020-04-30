35cede4f5bb5f60ddb7e54bc9721367c1c4e2300
chowder23 <benjamin2333@gmail.com>
2020-04-30 09:59:35 +0200

    d

--- src/main/java/hu/oe/nik/szfmv/automatedcar/systemcomponents/Ultrasonic.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/systemcomponents/Ultrasonic.java
@@ -2,6 +2,7 @@ package hu.oe.nik.szfmv.automatedcar.systemcomponents;
 
 import hu.oe.nik.szfmv.automatedcar.AutomatedCar;
 import hu.oe.nik.szfmv.automatedcar.model.World;
+import hu.oe.nik.szfmv.automatedcar.model.WorldObject;
 import hu.oe.nik.szfmv.automatedcar.virtualfunctionbus.VirtualFunctionBus;
 import hu.oe.nik.szfmv.automatedcar.virtualfunctionbus.packets.visualization.UltrasoundDisplayStatePacket;
 import hu.oe.nik.szfmv.automatedcar.virtualfunctionbus.packets.visualization.UltrasoundsVisualizationPacket;
@@ -35,13 +36,14 @@ public class Ultrasonic {
     private final static Point FRONT_LEFT_SENSOR = new Point(-40, -90);
     private final static Point REAR_RIGHT_SENSOR = new Point(40, 90);
     private final static Point REAR_LEFT_SENSOR = new Point(-40, 90);
-    
+
     private final static UltrasoundsVisualizationPacket ultrasoundsVisualizationPacket = new UltrasoundsVisualizationPacket();
     private final static UltrasoundDisplayStatePacket ultrasoundDisplayPacket = new UltrasoundDisplayStatePacket();
     private final VirtualFunctionBus virtualFunctionBus;
     private final AutomatedCar automatedCar;
     private final World world;
     private List<Sensor> sensors = new ArrayList<>();
+    private List<WorldObject> collidableObjectsInRange = new ArrayList<>();
 
     public Ultrasonic(VirtualFunctionBus virtualFunctionBus, AutomatedCar automatedCar, World world) {
         this.virtualFunctionBus = virtualFunctionBus;
@@ -50,21 +52,27 @@ public class Ultrasonic {
         sensors.add(new Sensor(this.virtualFunctionBus, this.automatedCar, this.world, FRONT_RIGHT_SENSOR,
                 UltrasoundPositions.FRONT_RIGHT, ultrasoundDisplayPacket, ultrasoundsVisualizationPacket));
         sensors.add(new Sensor(this.virtualFunctionBus, this.automatedCar, this.world, FRONT_LEFT_SENSOR,
-               UltrasoundPositions.FRONT_LEFT, ultrasoundDisplayPacket, ultrasoundsVisualizationPacket));
+                UltrasoundPositions.FRONT_LEFT, ultrasoundDisplayPacket, ultrasoundsVisualizationPacket));
         sensors.add(new Sensor(this.virtualFunctionBus, this.automatedCar, this.world, REAR_RIGHT_SENSOR,
                 UltrasoundPositions.REAR_RIGHT, ultrasoundDisplayPacket, ultrasoundsVisualizationPacket));
         sensors.add(new Sensor(this.virtualFunctionBus, this.automatedCar, this.world, REAR_LEFT_SENSOR,
-                UltrasoundPositions.REAR_LEFT, ultrasoundDisplayPacket, ultrasoundsVisualizationPacket)); 
+                UltrasoundPositions.REAR_LEFT, ultrasoundDisplayPacket, ultrasoundsVisualizationPacket));
         sensors.add(new Sensor(this.virtualFunctionBus, this.automatedCar, this.world, FRONT_RIGHT_SENSOR,
                 UltrasoundPositions.FRONT_RIGHT_SIDE, ultrasoundDisplayPacket, ultrasoundsVisualizationPacket));
         sensors.add(new Sensor(this.virtualFunctionBus, this.automatedCar, this.world, FRONT_LEFT_SENSOR,
-               UltrasoundPositions.FRONT_LEFT_SIDE, ultrasoundDisplayPacket, ultrasoundsVisualizationPacket));
+                UltrasoundPositions.FRONT_LEFT_SIDE, ultrasoundDisplayPacket, ultrasoundsVisualizationPacket));
         sensors.add(new Sensor(this.virtualFunctionBus, this.automatedCar, this.world, REAR_RIGHT_SENSOR,
                 UltrasoundPositions.REAR_RIGHT_SIDE, ultrasoundDisplayPacket, ultrasoundsVisualizationPacket));
         sensors.add(new Sensor(this.virtualFunctionBus, this.automatedCar, this.world, REAR_LEFT_SENSOR,
                 UltrasoundPositions.REAR_LEFT_SIDE, ultrasoundDisplayPacket, ultrasoundsVisualizationPacket));
-        
-              
+
+
     }
 
+    private void getAllCollidablesInRange() {
+        collidableObjectsInRange = new ArrayList<>();
+        for (Sensor sensor : sensors) {
+            
+        }
+    }
 }
